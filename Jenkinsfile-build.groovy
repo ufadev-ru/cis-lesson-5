b8f1e2c0f716d1cc48532d178cd792d5e46d14cd
Мельников Андрей Александрович <melnikov.aaleksan@devzone.local>
2024-12-04 10:01:12 +0500

    *

---
+++ Jenkinsfile-build.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                steps {
+                    echo "===== Build image frontend-core ====="
+                    sh """
+                    docker build -t $REGISTRY/$OCP_NAMESPACE/$OCP_APP_NAME-frontend-core:$APP_VERSION \
+                                 --no-cache \
+                                 --target final \
+                                 --build-arg NODEJS__BASE_IMAGE=$RH_REGISTRY/ubi8/nodejs-18:latest \
+                                 --build-arg NGINX__BASE_IMAGE=$RH_REGISTRY/ubi8/nginx-122:latest \
+                                 --build-arg FRONTEND__NPM_EXTERNAL_DSO=$REPO_NODEJS \
+                                 --build-arg APP_VERSION=$APP_VERSION \
+                                 -f $DOCKERFILE_PATH \
+                                 .
+                    """
+
+                    echo "===== push image frontend-core ====="
+                    doDocker("push", ["registry": "$REGISTRY", "registry_path": "$OCP_NAMESPACE", "image_name": "$OCP_APP_NAME-frontend-core", "image_tag": "$APP_VERSION"])
+                }
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+                // Команды для тестирования
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+                // Команды для развертывания
+            }
+        }
+    }
+}
